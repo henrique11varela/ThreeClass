2ab031976bcec1b189b6adbfee89d6c7b9761bc1
henrique11varela <henrique11varela@gmail.com>
2024-05-07 23:43:33 +0100

    -5

--- src/app/pages/orientation/orientation.component.ts
+++ src/app/pages/orientation/orientation.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { renderer, getOrientation } from "./three/file";
+import { renderer, orientationObj } from "./three/file";
 @Component({
   selector: 'app-orientation',
   templateUrl: './orientation.component.html',
@@ -7,9 +7,20 @@ import { renderer, getOrientation } from "./three/file";
 })
 export class OrientationComponent implements AfterViewInit {
   @ViewChild('orientation') orientationElement!: ElementRef<HTMLDivElement>
-  public getOrientation = () => getOrientation()
+  // public orientationObj: any = {
+  //   alpha: 0,
+  //   gamma: 0,
+  //   beta: 0,
+  // }
+  public orientationObjIn = orientationObj
 
   ngAfterViewInit(): void {
     this.orientationElement.nativeElement.appendChild(renderer.domElement)
+    // setInterval(()=>{
+    //   const temp = getOrientation()
+    //   this.orientationObj.alpha = temp.alpha
+    //   this.orientationObj.gamma = temp.gamma
+    //   this.orientationObj.beta = temp.beta
+    // }, 100)
   }
 }
--- src/app/pages/orientation/three/file.ts
+++ src/app/pages/orientation/three/file.ts
@@ -31,7 +31,7 @@ const cube = new THREE.Mesh(geometry, material)
 
 scene.add(cube)
 
-const orientation: any = {
+const orientationObj: any = {
   alpha: 0,
   gamma: 0,
   beta: 0,
@@ -52,16 +52,16 @@ if (window.DeviceOrientationEvent) {
 }
 
 const handleOrientationEvent = (frontToBack: number, leftToRight: number, rotateDegrees: number) => {
-  orientation.alpha = rotateDegrees
-  orientation.gamma = leftToRight
-  orientation.beta = frontToBack
+  orientationObj.alpha = rotateDegrees
+  orientationObj.gamma = leftToRight
+  orientationObj.beta = frontToBack
   // cube.rotation.set(frontToBack, rotateDegrees, leftToRight)
 };
 
 animations.push(function (deltaTime: number) {
-  cube.rotation.x = orientation.beta * Math.PI / 180
-  cube.rotation.y = orientation.alpha * Math.PI / 180
-  cube.rotation.z = orientation.gamma * Math.PI / 180
+  cube.rotation.x = orientationObj.beta * Math.PI / 180
+  cube.rotation.y = orientationObj.alpha * Math.PI / 180
+  cube.rotation.z = orientationObj.gamma * Math.PI / 180
 })
 
 
@@ -79,10 +79,10 @@ function animate(time: number) {
 renderer.setAnimationLoop(animate)
 
 function getOrientation() {
-  return { ...orientation }
+  return { ...orientationObj }
 }
 
 export {
   renderer,
-  getOrientation
+  orientationObj
 }
