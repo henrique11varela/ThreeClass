35d1dcc4ac00758ba5009965e0ca9c68b364fd4b
henrique11varela <henrique11varela@gmail.com>
2024-05-07 23:36:33 +0100

    -4

--- src/app/pages/orientation/three/file.ts
+++ src/app/pages/orientation/three/file.ts
@@ -22,7 +22,7 @@ scene.add(aLight)
 
 // const material = new THREE.MeshLambertMaterial(materialOptions)
 const material = new THREE.MeshLambertMaterial({
-    color: 0x888888
+  color: 0x888888
 })
 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
@@ -32,57 +32,57 @@ const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
 const orientation: any = {
-    alpha: 0,
-    gamma: 0,
-    beta: 0,
+  alpha: 0,
+  gamma: 0,
+  beta: 0,
 }
 
 if (window.DeviceOrientationEvent) {
-    window.addEventListener(
-        "deviceorientation",
-        (event) => {
-            const rotateDegrees: number = Number(event.alpha); // alpha: rotation around z-axis
-            const leftToRight: number = Number(event.gamma); // gamma: left to right
-            const frontToBack: number = Number(event.beta); // beta: front back motion
+  window.addEventListener(
+    "deviceorientation",
+    (event) => {
+      const rotateDegrees: number = Number(event.alpha); // alpha: rotation around z-axis
+      const leftToRight: number = Number(event.gamma); // gamma: left to right
+      const frontToBack: number = Number(event.beta); // beta: front back motion
 
-            handleOrientationEvent(Math.floor(frontToBack), Math.floor(leftToRight), Math.floor(rotateDegrees));
-        },
-        true,
-    );
+      handleOrientationEvent(Math.floor(frontToBack), Math.floor(leftToRight), Math.floor(rotateDegrees));
+    },
+    true,
+  );
 }
 
 const handleOrientationEvent = (frontToBack: number, leftToRight: number, rotateDegrees: number) => {
-    orientation.alpha = rotateDegrees
-    orientation.gamma = leftToRight
-    orientation.beta = frontToBack
-    // cube.rotation.set(frontToBack, rotateDegrees, leftToRight)
+  orientation.alpha = rotateDegrees
+  orientation.gamma = leftToRight
+  orientation.beta = frontToBack
+  // cube.rotation.set(frontToBack, rotateDegrees, leftToRight)
 };
 
 animations.push(function (deltaTime: number) {
-    cube.rotation.x = orientation.beta * Math.PI / 180
-    cube.rotation.y = orientation.alpha * Math.PI / 180
-    cube.rotation.z = orientation.gamma * Math.PI / 180
+  cube.rotation.x = orientation.beta * Math.PI / 180
+  cube.rotation.y = orientation.alpha * Math.PI / 180
+  cube.rotation.z = orientation.gamma * Math.PI / 180
 })
 
 
 let lastFrameTime: number = 0
 
 function animate(time: number) {
-    const deltaTime: number = (time - lastFrameTime) / 1000
-    lastFrameTime = time
-    animations.forEach((item: Function) => {
-        item(deltaTime)
-    })
-    renderer.render(scene, camera)
+  const deltaTime: number = (time - lastFrameTime) / 1000
+  lastFrameTime = time
+  animations.forEach((item: Function) => {
+    item(deltaTime)
+  })
+  renderer.render(scene, camera)
 }
 
 renderer.setAnimationLoop(animate)
 
 function getOrientation() {
-    return orientation
+  return { ...orientation }
 }
 
 export {
-    renderer,
-    getOrientation
+  renderer,
+  getOrientation
 }
