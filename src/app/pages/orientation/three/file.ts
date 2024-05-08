7057d5a3702717792b5fe5884560f6a1ed99c8eb
Henrique Varela <henrique.varela@nomadtech.eu>
2024-05-08 15:18:27 +0100

    pos8

--- src/app/pages/orientation/three/file.ts
+++ src/app/pages/orientation/three/file.ts
@@ -90,8 +90,8 @@ animations.push(function (deltaTime: number) {
     const pos: [number, number, number] = [0, 10, 0]
     const h = CAMERA_DISTANCE * Math.sin(beta)
     pos[1] = CAMERA_DISTANCE * Math.cos(beta)
-    pos[2] = h * Math.sin(alpha)
-    pos[0] = h * Math.cos(alpha)
+    pos[2] = h * Math.sin(alpha) * -1
+    pos[0] = h * Math.cos(alpha) * -1
     camera.position.set(...pos)
     camera.rotation.set(beta - Math.PI / 2, gamma, (alpha * 0))
 })
