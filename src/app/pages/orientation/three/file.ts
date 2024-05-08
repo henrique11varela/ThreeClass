6b7b12dbb61af5f7383ea5480eae85953fefa188
Henrique Varela <henrique.varela@nomadtech.eu>
2024-05-08 09:53:53 +0100

    h

--- src/app/pages/orientation/three/file.ts
+++ src/app/pages/orientation/three/file.ts
@@ -22,7 +22,7 @@ scene.add(aLight)
 
 // const material = new THREE.MeshLambertMaterial(materialOptions)
 const material = new THREE.MeshLambertMaterial({
-  color: 0x888888
+    color: 0x888888
 })
 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
@@ -32,65 +32,65 @@ const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
 const orientation: any = {
-  alpha: 0,
-  gamma: 0,
-  beta: 0,
+    alpha: 0,
+    gamma: 0,
+    beta: 0,
 }
 
 if (window.DeviceOrientationEvent) {
-  window.addEventListener(
-    "deviceorientation",
-    (event) => {
-      const rotateDegrees: number = Number(event.alpha); // alpha: rotation around z-axis
-      const leftToRight: number = Number(event.gamma); // gamma: left to right
-      const frontToBack: number = Number(event.beta); // beta: front back motion
+    window.addEventListener(
+        "deviceorientation",
+        (event) => {
+            const rotateDegrees: number = Number(event.alpha); // alpha: rotation around z-axis
+            const leftToRight: number = Number(event.gamma); // gamma: left to right
+            const frontToBack: number = Number(event.beta); // beta: front back motion
 
-      handleOrientationEvent(Math.floor(frontToBack), Math.floor(leftToRight), Math.floor(rotateDegrees));
-    },
-    true,
-  );
+            handleOrientationEvent(Math.floor(frontToBack), Math.floor(leftToRight), Math.floor(rotateDegrees));
+        },
+        true,
+    );
 }
 
 const handleOrientationEvent = (frontToBack: number, leftToRight: number, rotateDegrees: number) => {
-  orientation.alpha = rotateDegrees
-  orientation.gamma = leftToRight
-  orientation.beta = frontToBack
-  // cube.rotation.set(frontToBack, rotateDegrees, leftToRight)
+    orientation.alpha = rotateDegrees
+    orientation.gamma = leftToRight
+    orientation.beta = frontToBack
+    // cube.rotation.set(frontToBack, rotateDegrees, leftToRight)
 };
 
 const CAMERA_DISTANCE = 10
 
 animations.push(function (deltaTime: number) {
-  camera.rotation.x = orientation.beta * (Math.PI / 180)
-  camera.rotation.y = orientation.alpha * (Math.PI / 180)
-  camera.rotation.z = orientation.gamma * (Math.PI / 180)
-  const pos: [number, number, number] = [0, 0, 0]
-  const h = CAMERA_DISTANCE * Math.sin(camera.rotation.x)
-  pos[1] = CAMERA_DISTANCE * Math.cos(camera.rotation.x)
-  pos[2] = CAMERA_DISTANCE * Math.sin(camera.rotation.y)
-  pos[0] = CAMERA_DISTANCE * Math.cos(camera.rotation.y)
-  camera.position.set(...pos)
+    camera.rotation.x = orientation.beta * (Math.PI / 180)
+    camera.rotation.y = orientation.alpha * (Math.PI / 180)
+    camera.rotation.z = orientation.gamma * (Math.PI / 180)
+    const pos: [number, number, number] = [0, 0, 0]
+    const h = CAMERA_DISTANCE * Math.sin(camera.rotation.x)
+    pos[1] = CAMERA_DISTANCE * Math.cos(camera.rotation.x)
+    pos[2] = h * Math.sin(camera.rotation.y)
+    pos[0] = h * Math.cos(camera.rotation.y)
+    camera.position.set(...pos)
 })
 
 
 let lastFrameTime: number = 0
 
 function animate(time: number) {
-  const deltaTime: number = (time - lastFrameTime) / 1000
-  lastFrameTime = time
-  animations.forEach((item: Function) => {
-    item(deltaTime)
-  })
-  renderer.render(scene, camera)
+    const deltaTime: number = (time - lastFrameTime) / 1000
+    lastFrameTime = time
+    animations.forEach((item: Function) => {
+        item(deltaTime)
+    })
+    renderer.render(scene, camera)
 }
 
 renderer.setAnimationLoop(animate)
 
 function getOrientation() {
-  return { ...orientation }
+    return { ...orientation }
 }
 
 export {
-  renderer,
-  getOrientation
+    renderer,
+    getOrientation
 }
